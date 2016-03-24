a6d69d5fbb3b2374022ac48f9e7c0376fa6e3ad9
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2016-03-24 22:33:25 +0000

    Stars working correctly

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,46 +39,64 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function generateStar(){
-    var minWidth = 20;
-    var maxWidth = 70;
+function generateStar(col){
+    var minWidth = 10;
+    var maxWidth = 50;
     
     var minTilt = -6;
     var maxTilt = 18;
     
     var width = getRandomArbitrary(minWidth, maxWidth);
-    var posx = (Math.random() * ($(document).width() - width)).toFixed();
-    var posy = (Math.random() * ($(document).height() - width)).toFixed();
+    var delay = getRandomArbitrary(1000, 9000);
+    var delaySmall = getRandomArbitrary(0, 500);
+    var fade = getRandomArbitrary(700, 1400);
+    var posy = (Math.random() * (($(document).height() / 2) - width)).toFixed();
     var rotation = getRandomArbitrary(minTilt, maxTilt);
     var img = $('<img class="star">');
     img.attr('src', 'images/star.svg');
     img.css({
+        'display' : 'none',
         'position': 'absolute',
         'width': width + 'px',
-        'left': posx + 'px',
         'top': posy + 'px',
-        'display' : 'none',
         'transform': 'rotate('+rotation+'deg)'
     });
     // $(img, img1).appendTo( '.canvas' ).fadeIn(500).delay(1000).fadeOut(500, function(){
     //   $(this).remove();
     //   generateStar();
     // });
-    $(img).appendTo( '.canvas' ).fadeIn(500).delay(1000);
+    $(img).appendTo(col).delay(delaySmall).fadeIn(fade).delay(delay).fadeOut(delay, function(){
+    $(this).remove();
+        generateStar(col);
+    });
     
 }
 
 $(document).ready(function(){
     console.log('ready');
-    generateStar();
-    generateStar();
-    generateStar();
-    generateStar();
-    generateStar();
-    generateStar();
-    generateStar();
-    generateStar();
-    generateStar();
-    generateStar();
+    generateStar('.top12');
+    generateStar('.top1');
+    generateStar('.top11');
+    generateStar('.top2');
+    generateStar('.top10');
+    generateStar('.top3');
+    generateStar('.top9');
+    generateStar('.top4');
+    generateStar('.top8');
+    generateStar('.top5');
+    generateStar('.top7');
+    generateStar('.top6');
+    generateStar('.top6');
+    generateStar('.top7');
+    generateStar('.top5');
+    generateStar('.top8');
+    generateStar('.top4');
+    generateStar('.top9');
+    generateStar('.top3');
+    generateStar('.top10');
+    generateStar('.top2');
+    generateStar('.top11');
+    generateStar('.top1');
+    generateStar('.top12');
 
 });
